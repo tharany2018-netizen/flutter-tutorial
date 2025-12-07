08a6682cdc3429d2f5f7feb1f7883d9c438b9a0e
tharany2018-netizen <tharany2018@gmail.com>
2025-12-07 21:52:05 +0530

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text("Hello Bestie!")),
+        body: Center(
+          child: Text(
+            "Welcome to Flutter!",
+            style: TextStyle(fontSize: 22),
+          ),
+        ),
+      ),
+    );
+  }
+}
